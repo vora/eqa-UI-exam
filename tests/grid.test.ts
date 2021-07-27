7e5f68f565dd87d764c98cb21825feb8291474be
Lucky <something@something.com>
2021-07-28 03:05:27 +0530

    updated wdio to latest version

--- tests/grid.test.ts
+++ tests/grid.test.ts
@@ -14,50 +14,57 @@ describe("Load AG Grid", () => {
   ];
   const filterNames = ["Gil", "Tony", "Isabella", "Poppy", "aa"];
 
-  it("Should display the grids", () => {
-    GridPage.mainGrid.waitForDisplayed();
+  it("Should display the grids", async () => {
+    await (await GridPage.mainGrid).waitForDisplayed();
 
     /**
      * I'm using forEach here for readability purposes
      * The function iterates through the element array
      * and checks if element is displayed.
      */
-    GridPage.columnHeader.forEach((el) => {
-      expect(el.isDisplayed()).to.eql(true);
+    (await GridPage.columnHeader).forEach(async (el) => {
+      expect(await el.isDisplayed()).to.eql(true);
     });
   });
 
-  it("Should Validate primary column headings", () => {
+  it("Should Validate primary column headings", async () => {
     /**
      * I'm storing the expected result in an array,
      * iterating in a forEach and using if condition to filter only the expected result
      * as it fetches empty string as result
      */
 
-    GridPage.columnHeader.forEach((head, i = 0) => {
-      if (head.getText() != "") {
+    (await GridPage.columnHeader).forEach(async (head, i = 0) => {
+      if ((await head.getText()) != "") {
         expect(head.getText()).to.eql(columnHeaders[i]);
       }
     });
   });
 
-  it("Should Validate primary column headings are available in column sidebar options", () => {
+  it("Should Validate primary column headings are available in column sidebar options", async () => {
     /**
      * I am storing Primary column header and secondary column headers text value as array of string
      * using map and filter is use to remove empty strings.
      * I am using assert oneOf in forEach to check each primary column header is present in the side column header.
      *
      */
-    const primaryColumnHeaders = GridPage.columnHeader;
-    const filteredHead = primaryColumnHeaders
-      .filter((a) => a.getText() != "")
-      .map((b) => b.getText());
-
-    GridPage.sideColumnIcon.click();
-    const sideColumnHeaders = GridPage.sideColumnHeaders;
-    const filteredSideHeaders = sideColumnHeaders
-      .filter((a) => a.getText())
-      .map((b) => b.getText());
+    const primaryColumnHeaders = await GridPage.columnHeader;
+    const filteredHead = [];
+    for (let i = 0; i < primaryColumnHeaders.length; i++) {
+      const text = await primaryColumnHeaders[i].getText();
+      if (text) {
+        filteredHead.push(text);
+      }
+    }
+    (await GridPage.sideColumnIcon).click();
+    const sideColumnHeaders = await GridPage.sideColumnHeaders;
+    const filteredSideHeaders = [];
+    for (let i = 0; i < sideColumnHeaders.length; i++) {
+      const text = await sideColumnHeaders[i].getText();
+      if (text) {
+        filteredSideHeaders.push(text)
+      }
+    }
 
     filteredHead.forEach((ele) => {
       assert.oneOf(
@@ -68,7 +75,7 @@ describe("Load AG Grid", () => {
     });
   });
 
-  it("Should validate filtering name returns result", () => {
+  it("Should validate filtering name returns result", async () => {
     /**
      * I have used for loop to search different names
      * for each iteration, the value is entered using setValue method.
@@ -78,9 +85,9 @@ describe("Load AG Grid", () => {
      */
     for (let i = 0; i <= filterNames.length - 1; i++) {
       try {
-        GridPage.nameSearchBox.setValue(filterNames[i]);
-        GridPage.nameResult.waitForDisplayed();
-        expect(GridPage.nameResult.isDisplayed()).to.be.true;
+        (await GridPage.nameSearchBox).setValue(filterNames[i]);
+        (await GridPage.nameResult).waitForDisplayed();
+        expect((await GridPage.nameResult).isDisplayed()).to.be.true;
       } catch (e) {
         console.log(`${filterNames[i]} doesnot fetch any result`);
       }
